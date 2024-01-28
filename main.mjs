26ebe45ab4dece6ba6305e2f3b46ad08a8167d61
suryanshu0-coder <110730590+suryanshu0-coder@users.noreply.github.com>
2024-01-28 20:30:05 +0545

    add dark mode

--- main.mjs
+++ main.mjs
@@ -5,6 +5,21 @@ import {
   createPreviousButton,
 } from "./buttons.mjs";
 
+const toggleMode = document.getElementById("toggleMode");
+let isDarkMode = false;
+const switchMode = document.getElementById("switch");
+switchMode.addEventListener("click", () => {
+  isDarkMode = !isDarkMode;
+  if (isDarkMode) {
+    toggleMode.href = "dark.css";
+    switchMode.innerHTML = `<i class="fas fa-sun"></i>`;
+    switchMode.style.transition = "background-color 0.3s ease";
+  } else {
+    toggleMode.href = "light.css";
+    switchMode.innerHTML = `<i class="fas fa-moon"></i>`;
+    switchMode.style.transition = "background-color 0.3s ease";
+  }
+});
 let currentQuestionIndex = 0;
 async function fetchData() {
   try {
@@ -35,9 +50,9 @@ function displayQuestion(data) {
   options.forEach((option, index) => {
     optionsHTML += `
       <div id="contents">
-        <input type="checkbox" value="${option.id}" id="opt${
+        <input type="radio" value="${option.id}" id="opt${
       index + 1
-    }" aria-label="opt-${index + 1}">
+    }" name="option" aria-label="opt-${index + 1}">
         <label htmlFor="opt${index + 1}" id="op${index + 1}">${
       option.text
     }</label>
@@ -48,9 +63,10 @@ function displayQuestion(data) {
 }
 const nextButton = createNexttButton();
 const previousButton = createPreviousButton();
+const submitBtn = createSubmitButton();
+submitBtn.style.display = "none";
 nextButton.style.display = "none";
 previousButton.style.display = "none";
-
 const startBtn = createStartButton();
 startBtn.addEventListener("click", () => {
   fetchData().then((data) => {
@@ -59,20 +75,46 @@ startBtn.addEventListener("click", () => {
     previousButton.style.display = "block";
     displayQuestion(data);
 
+    if (currentQuestionIndex === 0) {
+      previousButton.style.display = "none";
+    }
+    if (currentQuestionIndex === data.questions.length - 1) {
+      nextButton.style.display = "none";
+      submitBtn.style.display = "block";
+    }
+
+    const textDiv = document.getElementById("text");
     nextButton.addEventListener("click", () => {
       if (currentQuestionIndex < data.questions.length - 1) {
         currentQuestionIndex++;
+        textDiv.textContent = `${
+          10 - currentQuestionIndex
+        }/10 questions remaining`;
         displayQuestion(data);
+        previousButton.style.display = "block";
+      }
+
+      // Check if it's the last question after moving to the next
+      if (currentQuestionIndex === data.questions.length - 1) {
+        nextButton.style.display = "none";
+        submitBtn.style.display = "block";
       }
     });
 
     previousButton.addEventListener("click", () => {
       if (currentQuestionIndex > 0) {
         currentQuestionIndex--;
+        textDiv.textContent = `${Math.abs(
+          currentQuestionIndex - 10
+        )}/10 questions remaining`;
         displayQuestion(data);
       }
-    }).catch((error) => {
-      throw new Error("Error showing the message" , error);
+
+      // Check if it's not the last question after moving to the previous
+      if (currentQuestionIndex < data.questions.length - 1) {
+        nextButton.style.display = "block";
+        submitBtn.style.display = "none";
+      }
     });
   });
 });
